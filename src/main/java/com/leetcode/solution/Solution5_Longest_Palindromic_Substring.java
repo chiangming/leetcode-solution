b04d41dab8ffb68f59428dd67fa049aa8f31949d
chiangming <1245333655@qq.com>
2019-04-11 17:20:10 +0800

    [LeetCode][expansion] 动态规划算法

--- src/main/java/com/leetcode/solution/Solution5_Longest_Palindromic_Substring.java
+++ src/main/java/com/leetcode/solution/Solution5_Longest_Palindromic_Substring.java
@@ -26,4 +26,24 @@ public class Solution5_Longest_Palindromic_Substring {
 
     }
 
+    //动态规划算法
+    //就是将之前求的值保存起来，方便后边的计算，使得一些多余的计算消失了。并且在动态规划中，通过观察数组的利用情况，从而降低了空间复杂度。
+    public String longestPalindrome2(String s) {
+        int length = s.length();
+        boolean[][] P = new boolean[length][length];
+        int maxLen = 0;
+        String maxPal = "";
+        for (int len = 1; len <= length; len++) //遍历所有的长度
+            for (int start = 0; start < length; start++) {
+                int end = start + len - 1;
+                if (end >= length) //下标已经越界，结束本次循环
+                    break;
+                P[start][end] = (len == 1 || len == 2 || P[start + 1][end - 1]) && s.charAt(start) == s.charAt(end); //长度为 1 和 2 的单独判断下
+                if (P[start][end] && len > maxLen) {
+                    maxPal = s.substring(start, end + 1);
+                }
+            }
+        return maxPal;
+    }
+
 }
